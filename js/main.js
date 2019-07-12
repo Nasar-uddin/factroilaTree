f399b784c208507f8d2d9d97007682b44f8e10df
Nasar uddin <nasar00708@gmail.com>
2019-07-12 23:51:50 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,18 +8,13 @@ const rad = Math.PI / 180;
 
 class Tree {
     constructor(angle) {
-        this.startX = 0
-        this.startY = 0
         this.color = "rgb(30,180,55)";
+        this.angle = angle;
         this.branchCount = 0;
-        this.x = NaN
-        this.y = NaN
-        this.r_angle = angle
-        this.l_angle = -angle
+        this.l_angle_scale = 1
+        this.r_angle_scale = 1
     }
     draw(startX,startY,len,angle) {
-        // this.startX = startX
-        // this.startY = startY
         c.beginPath();
         c.save();
         c.translate(startX, startY);
@@ -32,34 +27,51 @@ class Tree {
         c.closePath()
         c.strokeStyle = this.color;
         c.stroke();
-        if (len < 20) {
+        if (len < 30) {
             c.beginPath()
-            c.arc(0,-len,5,0,360*rad);
+            c.arc(0,-len,2,0,360*rad);
             c.fillStyle = this.color;
             c.fill();
             c.restore();
             return;
         }
-        this.draw(0, -len, len * 0.8, this.l_angle);
-        this.draw(0, -len, len * 0.8,this.r_angle);
+        this.draw(0,-len,len*0.75,-this.angle*this.l_angle_scale);
+        this.draw(0, -len,len * 0.75,this.angle*this.r_angle_scale);
         c.restore();
-        window.requestAnimationFrame(this.draw)
+        
     }
-    interaction(){
-        canvas.onmousemove = (e)=>{
-            this.x = e.x
-            this.y = e.y
-            if(e.y<iw/3){
-                this.r_angle = this.r_angle*0.8
-                this.l_angle = this.l_angle*0.8
-            }
-            console.log(`mX ${this.x} mY ${this.y} angle ${this.r_angle} ${this.l_angle} sx ${this.startX}`)
-        }
+    update(left_angle_scale,right_angle_scale){
+        this.l_angle_scale = left_angle_scale
+        this.r_angle_scale = right_angle_scale
     }
 }
-var t1 = new Tree(20);
-t1.draw(iw/3,ih,90);
 
-var t2 = new Tree(20);
-t2.draw(iw/1.5,ih,70);
-t2.interaction()
+var t1 = new Tree(15);
+// var xx = 1;
+// var speed  = 0.005;
+// var sign = -1;
+var x_ratio = 0;
+var y_ratio = 1;
+function animate(){
+    // if(xx>=1.5){
+    //     sign = -1
+    // }else if(xx<0.8){
+    //     sign = 1
+    // }
+    // xx = xx+speed*sign
+    c.fillStyle = '#ffffff';
+    y_ratio = 1-x_ratio;
+    c.fillRect(0,0,iw,ih);
+    t1.update(x_ratio+1,y_ratio);
+    t1.draw(iw/2,ih,100,0);
+    requestAnimationFrame(animate)
+}
+animate()
+canvas.onmousemove = function(e){
+    // console.log(`x:${e.x} y:${e.y}`);
+    x_ratio = 1-(e.x/(iw/2))
+    if(e.x<iw/2){
+        // x_ratio = 1-(e.x/(iw/2))
+        // console.log("X r is "+x_ration+" iw is "+iw/2+" ex "+e.x)
+    }
+}
